09af3333dac5319709ef4f1d3a641a146af0528e
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-23 16:52:56 +0600

    client: possible routes are created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,51 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//Import all components
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+// root routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>,
+  },
+  {
+    path: '/register',
+    element: <Register></Register>,
+  },
+  {
+    path: '/password',
+    element: <Password></Password>,
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>,
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>,
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">App Login App</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 };
 
