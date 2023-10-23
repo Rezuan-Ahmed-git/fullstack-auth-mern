f3ef4f9835d1b0d7783a37348f1b825c9819fa47
rezuan <rezuan.ahmed.pro@gmail.com>
2023-10-23 18:34:15 +0600

    client: password component implement with validation

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,8 +1,33 @@
+/* eslint-disable no-useless-escape */
 import toast from 'react-hot-toast';
 
 /**validate login page username */
 export async function usernameValidate(values) {
   const errors = usernameVerify({}, values);
+
+  return errors;
+}
+
+/** validate password */
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+
+  return errors;
+}
+
+/** validate password */
+function passwordVerify(errors = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    errors.password = toast.error('Password Required...!');
+  } else if (values.password.includes(' ')) {
+    errors.password = toast.error('Wrong password');
+  } else if (values.password.length < 4) {
+    errors.password = toast.error('Password must be more than that.');
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error('Password must have special characters');
+  }
 
   return errors;
 }
