5b93799ee111b9ae5f840ec12f5bcf5f06e7618d
rezuan <rezuan.ahmed.pro@gmail.com>
2023-11-06 07:17:15 +0600

    feat: api request to client generateOTP, verifyOTP, and resetPassword are implemented

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-undef */
 import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
 /** Make API Requests */
 
@@ -68,5 +71,56 @@ export async function updateUser(response) {
     return Promise.resolve({ data });
   } catch (error) {
     return Promise.reject({ error: "Couldn't Update Profile..!" });
+  }
+}
+
+/** generate OTP */
+export async function generateOTP(username) {
+  try {
+    const {
+      data: { code },
+      status,
+    } = await axios.get('/api/generateOTP', { params: { username } });
+
+    if (status === 201) {
+      let {
+        data: { email },
+      } = await getUser({ username });
+      let text = `Your password Recovery OTP is ${code}. Verify and recover your password`;
+      await axios.post('/api/registerMail', {
+        username,
+        userEmail: email,
+        text,
+        subject: 'Password Recovery OTP',
+      });
+    }
+    return Promise.resolve(code);
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+/** verify OTP */
+export async function verifyOTP({ username, code }) {
+  try {
+    const { data, status } = await axios.get('/api/verifyOTP', {
+      params: { username, code },
+    });
+    return { data, status };
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+/** reset password */
+export async function resetPassword({ username, password }) {
+  try {
+    const { data, status } = await axios.put('/api/resetPassword', {
+      username,
+      password,
+    });
+    return Promise.resolve({ data, status });
+  } catch (error) {
+    return Promise.reject({ error });
   }
 }
